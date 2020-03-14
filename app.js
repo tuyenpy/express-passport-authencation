233ab3f6f1c32225ae527eefa837eee986fb5dbd
songthinhvuong87 <songthinhvuong87@gmail.com>
2020-03-14 11:04:42 +0700

    Build theme

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT||3000;
+const expressLayouts = require('express-ejs-layouts');
+
+//EJS - template engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 
 //Routers
 app.use('/', require('./routes/index'));
-app.use('/user', require('./routes/user'));
+app.use('/users', require('./routes/user'));
 
 app.listen(PORT, () => console.log(`Server is listening at ${PORT}`));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('', (req, res) => res.send("Welcome"));
+//Homepage
+router.get('', (req, res) => res.render('welcome'));
+
+//Dashboard
+router.get('/dashboard', (req, res) => res.render('dashboard'));
 
 module.exports = router;
